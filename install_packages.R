092964d433cfdb54efde3bb7fc5aadd6201ec79c
Anaih <anaihpp@hotmail.com>
2021-07-19 22:46:17 -0300

    add atualizacoes

---
+++ install_packages.R
@@ -0,0 +1,5 @@
+install.packages(c("remotes","gtrendsR","dplyr","ggplot2","wordcloud2",
+                   "stringr","RColorBrewer","ggrepel","rmarkdown"))
+remotes::install_github("anaih/prettydoc")
+
+
